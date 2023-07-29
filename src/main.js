9704fd9d0f5373d2eb29bfb0160a31a421dc2f63
Hakizimana Clement <h.clementoert@gmail.com>
2023-07-29 18:07:28 +0200

    complete choose us

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/header.css"; // component
 import "../styles/components/hero.css"; // component
+
+import "../styles/components/choose-us.css"; // component
 import "../styles/utils.css";
